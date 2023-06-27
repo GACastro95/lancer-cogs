247896286ce3fc11914dfca31420fe9375d9d77d
nLspriter <52107943+GACastro95@users.noreply.github.com>
2023-06-27 16:55:19 +0100

    Update avatar init

--- avatar/__init__.py
+++ avatar/__init__.py
@@ -1,8 +1,5 @@
-# This init is required for each cog.
-# Import your main class from the cog's folder.
 from .avatar import Avatar
 
 
-def setup(bot):
-    # Add the cog to the bot.
-    bot.add_cog(Avatar())
+async def setup(bot):
+    await bot.add_cog(Avatar())
