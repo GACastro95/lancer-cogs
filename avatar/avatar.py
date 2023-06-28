783503ffdc43f206c2605bbb87a0af0594aa012e
nLspriter <52107943+GACastro95@users.noreply.github.com>
2023-06-28 13:26:41 +0100

    Add subcommands to avatar command

--- avatar/avatar.py
+++ avatar/avatar.py
@@ -10,26 +10,45 @@ from redbot.core import commands
 class Avatar(commands.Cog):
     """Get user's avatar URL."""
 
-    @commands.command()
-    async def avatar(self, ctx, server=None, user: discord.Member=None):
-        """Returns user avatar URL.
+    @commands.group()
+    async def avatar(self, ctx: commands.Context) -> None:
+        """Returns user avatar URL."""
+        pass
+
+    @avatar.command(name="server")
+    async def _server_avatar(self, ctx, *, user: discord.Member=None):
+        """Returns server user avatar.
 
         User argument can be user mention, nickname, username, user ID.
         Default to yourself when no argument is supplied.
+        """
+        await self.get_avatar(ctx, "server", user)
+
+    @avatar.command(name="global")
+    async def _global_avatar(self, ctx, *, user: discord.Member=None):
+        """Returns the global user avatar.
+
+        User argument can be user mention, nickname, username, user ID.
+        Default to yourself when no argument is supplied.
+        """
+        await self.get_avatar(ctx, "global", user)
+
+    async def get_avatar(self, ctx, type, user: discord.Member):
+        """Gets the correct avatar type for the user
         """
         author = ctx.author
 
         if not user:
             user = author
 
-        if not server or server == "global":
+        if (type == "global"):
             avatar = user.avatar
-        elif server == "server":
+        elif (type == "server"):
             avatar = user.display_avatar
-            
+
         if (avatar.is_animated()):
             url = avatar.with_static_format("gif")
         else:
             url = avatar.with_static_format("png")
-
+        
         await ctx.send(f"{user}'s Avatar URL : {url}")
