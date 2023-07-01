a5793fa43fc5ceb01f47cd371d6d80decf13a0bc
nLspriter <52107943+GACastro95@users.noreply.github.com>
2023-07-01 13:42:58 +0100

    check tweet

--- vxtwitter/vxtwitter.py
+++ vxtwitter/vxtwitter.py
@@ -15,8 +15,10 @@ class VxTwitter(commands.Cog):
     async def validate_url(self, ctx, url):
         url_extract_pattern = "((https?):\/\/)?(www.)?twitter\.com(\/@?(\w){1,15})\/status\/[0-9]{19}"
         tweet = re.findall(url_extract_pattern, url)
+        print(tweet)
         if not tweet:
             await ctx.send("Tweet not found.")
         else:
+            print(tweet[0])
             vx = tweet[0].split("https://")
             await ctx.send(f"{vx[0]}vx{vx[1]}")
