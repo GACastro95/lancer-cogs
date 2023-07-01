05262314769f2268b5e25421b55565dc02830aa9
nLspriter <52107943+GACastro95@users.noreply.github.com>
2023-07-01 13:47:07 +0100

    check tweet extraction

--- vxtwitter/vxtwitter.py
+++ vxtwitter/vxtwitter.py
@@ -16,9 +16,9 @@ class VxTwitter(commands.Cog):
         url_extract_pattern = "((https?):\/\/)?(www.)?twitter\.com(\/@?(\w){1,15})\/status\/[0-9]{19}"
         tweet = re.findall(url_extract_pattern, url)
         print(tweet)
-        if not tweet:
-            await ctx.send("Tweet not found.")
-        else:
-            print(tweet[0])
-            vx = tweet[0].split("https://")
-            await ctx.send(f"{vx[0]}vx{vx[1]}")
+        # if not tweet:
+        #     await ctx.send("Tweet not found.")
+        # else:
+        #     print(tweet[0])
+        #     vx = tweet[0].split("https://")
+        #     await ctx.send(f"{vx[0]}vx{vx[1]}")
