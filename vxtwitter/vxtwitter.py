4011bac5c8919ca2dc842ed87678815e06340a57
nLspriter <52107943+GACastro95@users.noreply.github.com>
2023-07-01 13:19:01 +0100

    Initial VxTwitter commit

---
+++ vxtwitter/vxtwitter.py
@@ -0,0 +1,21 @@
+# Simple avatar URL fetch
+import discord
+import re
+from redbot.core import commands
+
+class Avatar(commands.Cog):
+    """Get user's avatar URL."""
+
+    @commands.hybrid_command()
+    async def twitter(self, ctx, url):
+        """Returns user avatar URL."""
+        self.validate_url(ctx, url)
+
+
+
+    async def validate_url(self, ctx, url):
+        url_pattern = "^https?:\\/\\/(?:www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b(?:[-a-zA-Z0-9()@:%_\\+.~#?&\\/=]*)$"     
+        await ctx.send(re.match(url_pattern, url))
+
+        url_extract_pattern = "https?:\\/\\/(?:www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b(?:[-a-zA-Z0-9()@:%_\\+.~#?&\\/=]*)"
+        await ctx.send(re.findall(url_extract_pattern, url))
