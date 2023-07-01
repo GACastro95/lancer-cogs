9d7dc2a7adb89dc0bdaf88b7f754e7dbd8b7bf4b
nLspriter <52107943+GACastro95@users.noreply.github.com>
2023-07-01 13:25:36 +0100

    remove match

--- vxtwitter/vxtwitter.py
+++ vxtwitter/vxtwitter.py
@@ -13,8 +13,5 @@ class VxTwitter(commands.Cog):
 
 
     async def validate_url(self, ctx, url):
-        url_pattern = "^https?:\\/\\/(?:www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b(?:[-a-zA-Z0-9()@:%_\\+.~#?&\\/=]*)$"     
-        await ctx.send(re.match(url_pattern, url))
-
         url_extract_pattern = "https?:\\/\\/(?:www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b(?:[-a-zA-Z0-9()@:%_\\+.~#?&\\/=]*)"
         await ctx.send(re.findall(url_extract_pattern, url))
