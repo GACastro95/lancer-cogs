d1e8972f2f126a0846e24b44ec9ded0ad3cbb6ec
Gabriel Castro <gabriel.alfonso.castro@gmail.com>
2023-10-14 23:48:46 +0100

    add missing parenthesis

--- fxtwitter/fxtwitter.py
+++ fxtwitter/fxtwitter.py
@@ -8,7 +8,7 @@ from typing import Optional
 class ButtonMenu(View):
     @discord.ui.button(style=discord.ButtonStyle.red, emoji="🗑")
     async def delete(self, interaction, button):
-        if interaction.user.id == interaction.original_response.author.id:
+        if interaction.user.id == interaction.original_response().author.id:
             await interaction.response.defer()
             await interaction.delete_original_response()
          
