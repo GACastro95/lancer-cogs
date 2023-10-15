f902f0fcfbf9aa04238b56dc1eea78ab182e093e
Gabriel Castro <gabriel.alfonso.castro@gmail.com>
2023-10-15 11:06:27 +0100

    Add instagram

--- fxtwitter/fxtwitter.py
+++ fxtwitter/fxtwitter.py
@@ -26,25 +26,25 @@ class FxTwitter(commands.Cog):
     """Converts twitter links to Fxtwitter links"""
     @commands.hybrid_command()
     async def twitter(self, ctx, url, download: Optional[bool] = False):
-        """Returns Fxtwitter Link."""
+        """Returns fxTwitter Link."""
         if download:
-                subst = "https://dl.fxtwitter.com"
+                subst = "dl.fxtwitter"
         else:
-            subst = "https://fxtwitter.com"
-        regex = r"((https?):\/\/)?(www.)?(x|twitter?)\.com(\/@?(\w){1,15})\/status\/[0-9]{19}"
+            subst = "fxtwitter"
+        regex = r"((https?):\/\/)?(www.)?(x|twitter)\.com(\/@?(\w){1,15})\/status\/[0-9]{19}"
         matches = re.search(regex, url)
         if matches:
-            regex_rm = r"((https?):\/\/)?(www.)?(x|twitter?)\.com"
+            regex_rm = r"(x|twitter)"
             result = re.sub(regex_rm, subst, url.split("?")[0], 1)
             menu = ButtonMenu(timeout=None, member=ctx.author)
             await ctx.send(result, view=menu)
         else:
-            await ctx.send("This is not a tweet", ephemeral=True)
+            await ctx.send("This is not a valid tweet", ephemeral=True)
 
     
     @commands.hybrid_command()
     async def tiktok(self, ctx, url):
-        """Returns Vxtiktok Link."""
+        """Returns fxTikTok Link."""
         regex = r"((https?):\/\/)?((vm|www?).)?(tiktok)\.com\/(@?.*?)\/(video)?(\/[0-9]{19})?"
         matches = re.search(regex, url)
         if matches:
@@ -53,4 +53,17 @@ class FxTwitter(commands.Cog):
             menu = ButtonMenu(timeout=None, member=ctx.author)
             await ctx.send(result, view=menu)
         else:
-            await ctx.send("This is not a tiktok", ephemeral=True)
+            await ctx.send("This is not a valid tiktok", ephemeral=True)
+
+    @commands.hybrid_command()
+    async def instagram(self, ctx, url):
+        """Returns ddInstagram Link."""
+        regex = r"((https?):\/\/)?((www?).)?(instagram)\.com\/(p|reel)\/(.*)\/.*?"
+        matches = re.search(regex, url)
+        if matches:
+            regex_rm = r"instagram"
+            result = re.sub(regex_rm, "ddinstagram", url.split("?")[0], 1)
+            menu = ButtonMenu(timeout=None, member=ctx.author)
+            await ctx.send(result, view=menu)
+        else:
+            await ctx.send("This is not a valid instagram post", ephemeral=True)
